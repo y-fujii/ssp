0e73062fcc7526527c1561121420577eec42ce29
y-fujii <y-fujii@localhost>
2012-05-11 16:06:12 +0900

    impl atan, atan2

--- ssp-math.hpp
+++ ssp-math.hpp
@@ -4,50 +4,46 @@
 #include <cmath>
 #include "ssp.hpp"
 
+namespace ssp {
+
+
 namespace detail {
 	#define USE_SSE2
 	#include "sse_mathfun.h"
 	#undef USE_SSE2
 }
 
-inline ssp::array<float, 4> sin( ssp::array<float, 4> const& x ) {
-	return ssp::array<float, 4>( detail::sin_ps( x._packed ) );
+inline array<float, 4> sin( array<float, 4> const& x ) {
+	return array<float, 4>( detail::sin_ps( x._packed ) );
 }
 
-inline ssp::array<float, 4> cos( ssp::array<float, 4> const& x ) {
-	return ssp::array<float, 4>( detail::cos_ps( x._packed ) );
+inline array<float, 4> cos( array<float, 4> const& x ) {
+	return array<float, 4>( detail::cos_ps( x._packed ) );
 }
 
-inline ssp::array<float, 4> exp( ssp::array<float, 4> const& x ) {
-	return ssp::array<float, 4>( detail::exp_ps( x._packed ) );
+inline array<float, 4> exp( array<float, 4> const& x ) {
+	return array<float, 4>( detail::exp_ps( x._packed ) );
 }
 
-inline ssp::array<float, 4> log( ssp::array<float, 4> const& x ) {
-	return ssp::array<float, 4>( detail::log_ps( x._packed ) );
+inline array<float, 4> log( array<float, 4> const& x ) {
+	return array<float, 4>( detail::log_ps( x._packed ) );
 }
 
 template<int N>
-inline ssp::array<float, N> asin_core(
-	ssp::array<float, N> const& x1,
-	ssp::array<float, N> const& x2
-) {
-	using namespace ssp;
-
+inline array<float, N> asin_core( array<float, N> const& x1, array<float, N> const& x2 ) {
 	array<float, N> z =
-	    ((((4.2163199048e-2f  * x2
-	      + 2.4181311049e-2f) * x2
-	      + 4.5470025998e-2f) * x2
-	      + 7.4953002686e-2f) * x2
+		((((4.2163199048e-2f  * x2
+		  + 2.4181311049e-2f) * x2
+		  + 4.5470025998e-2f) * x2
+		  + 7.4953002686e-2f) * x2
 		  + 1.6666752422e-1f) * x2 * x1
-	      + x1;
+		  + x1;
 
 	return where( x2 < 1e-8f, x1, z );
 }
 
 template<int N>
-inline ssp::array<float, N> asin( ssp::array<float, N> const& x ) {
-	using namespace ssp;
-
+inline array<float, N> asin( array<float, N> const& x ) {
 	array<float, N> a = abs( x );
 	array<int32_t, N> flag = a > 0.5f;
 	array<float, N> x2 = where( flag, 0.5f * (1.0f - a), a * a );
@@ -61,9 +57,7 @@ inline ssp::array<float, N> asin( ssp::array<float, N> const& x ) {
 }
 
 template<int N>
-inline ssp::array<float, N> acos( ssp::array<float, N> const& x ) {
-	using namespace ssp;
-
+inline array<float, N> acos( array<float, N> const& x ) {
 	array<float, N> a = abs( x );
 	array<int32_t, N> flag = a > 0.5f;
 	array<float, N> x2 = where( flag, 0.5f * (1.0f - a), a * a );
@@ -78,52 +72,42 @@ inline ssp::array<float, N> acos( ssp::array<float, N> const& x ) {
 	);
 }
 
-/*
-float atan( float x ) {
-	float sign = where( x < 0.0f, -1.0f, +1.0f );
-	float x    = where( x < 0.0f, -x   , +x    );
+template<int N>
+inline array<float, N> atan( array<float, N> const& x ) {
+	array<float, N> a = abs( x );
+	array<int32_t, N> flag0 = a > float( 3.0 * M_PI / 8.0 );
+	array<int32_t, N> flag1 = a > float( 1.0 * M_PI / 8.0 );
 
-	y = where(
-		x > (3.0 * M_PI / 8.0), (M_PI / 2.0),
-		x > (1.0 * M_PI / 8.0), (M_PI / 4.0),
-								0.0f
+	a = where(
+		flag0, -1.0f / a,
+		flag1, (a - 1.0f) / (a + 1.0f),
+		       a
 	);
 
-	x = where(
-		x > (3.0 * M_PI / 8.0), -1.0f / x,
-		x > (1.0 * M_PI / 8.0), (x - 1.0f) / (x + 1.0f),
-		                        x
+	array<float, N> a2 = a * a;
+	array<float, N> z =
+		(((8.05374449538e-2  * a2
+		 - 1.38776856032e-1) * a2
+		 + 1.99777106478e-1) * a2
+		 - 3.33329491539e-1) * a2 * a
+		 + a;
+
+	z = z + where(
+		flag0, array<float, N>( M_PI / 2.0 ),
+		flag1, array<float, N>( M_PI / 4.0 ),
+		       array<float, N>( 0.0f )
 	);
-
-	float z = x * x;
-	y += (((8.05374449538e-2  * z
-	      - 1.38776856032E-1) * z
-	      + 1.99777106478E-1) * z
-	      - 3.33329491539E-1) * z * x
-	      + x;
-
-	y = s * y;
-	return y;
-}
-
-float atan2( float y, float x ) {
-	float z = atanf( y / x );
-
-	z = where( y < 0.0f,
-			where( x < 0.0f, -M_PI + z, z ),
-			where( x < 0.0f, +M_PI + z, z )
-		);
-
-	z.assign_if( x == 0.0f, where(
-		y <  0.0f, -M_PI / 2.0,
-		y == 0.0f, 0.0f,
-		y >  0.0f, +M_PI / 2.0
-	) );
-
-	z.assign_if( y == 0.0f,
-		where( x < 0.0f, M_PI, 0.0f )
-	);
+	z = copysign( z, x );
 
 	return z;
 }
-*/
+
+template<int N>
+array<float, N> atan2( array<float, N> const& y, array<float, N> const& x ) {
+	array<float, N> z = atan( y / x );
+
+	return where( x > 0.0f, copysign( M_PI, y ) + z, z );
+}
+
+
+}
